ea81d879e28eb99ef2a959a55ff599d1cc82e018
akhilbabukorkandy <akhilbabu92@gmail.com>
2025-01-18 23:01:10 +0530

    bubble sort updated

--- com/dsa/test/BubbleSort.java
+++ com/dsa/test/BubbleSort.java
@@ -5,18 +5,18 @@ import java.util.Arrays;
 public class BubbleSort {
 
     public static void main(String[] args) {
-        int[] array=new int[]{1,3,34,2,4,6,5};
-       // System.out.println(array.length);
-        for (int i = 0; i < array.length-1; i++) {
-          //  System.out.println(i);
+        int[] array = new int[]{1, 3, 34, 2, 4, 6, 5};
+        // System.out.println(array.length);
+        for (int i = 0; i < array.length - 1; i++) {
+            //  System.out.println(i);
 
-            for (int j = 0; j < array.length-1; j++) {
-            //    System.out.println("j"+j);
-                if(array[j]<array[j+1]){
+            for (int j = 0; j < array.length - i - 1; j++) {
+                //    System.out.println("j"+j);
+                if (array[j] > array[j + 1]) {
 
-                    int temp=array[j];
-                    array[j]=array[j+1];
-                    array[j+1]=temp;
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
 
                 }
 
